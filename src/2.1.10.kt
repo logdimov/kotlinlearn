8ed8cea4bbb3f12b17afa29fa53bcbf54c98e6ad
Dmitry Logvinov <60686090+logdimov@users.noreply.github.com>
2020-10-15 21:44:47 +0300

    Add files via upload

---
+++ src/2.1.10.kt
@@ -0,0 +1,7 @@
+fun main() {
+// Работа с вводимыми данными
+    val firstname: String? = readLine()
+    val lastname: String? = readLine()
+
+    println("Hello ${firstname} ${lastname}!")
+}
---
+++ src/2.1.9.kt
@@ -0,0 +1,5 @@
+fun main() {
+// Чтение данных из строки. Тип данных у readline - Это .String? (именно ?)
+    val a = readLine()
+    println(a)
+}
